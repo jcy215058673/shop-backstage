8abd14f4103576d0ec8a624d8ae3b72f3999ebea
Jiang Chaoyang <215058673@qq.com>
2018-04-09 16:21:39 +0800

    jcy第二次提交

---
+++ shop-backstage-consumer/src/main/java/com/jk/controller/TreeController.java
@@ -0,0 +1,30 @@
+package com.jk.controller;
+
+import com.jk.model.Tree;
+import com.jk.service.ITreeService;
+import org.omg.CORBA.Object;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/4/8.
+ */
+@Controller
+@RequestMapping("tree")
+public class TreeController extends BaseController{
+
+    @Autowired
+    private ITreeService treeService;
+
+    @RequestMapping("/queryTreeList")
+    @ResponseBody
+    public void queryTreeList(HttpServletResponse response){
+        List<Tree> treeList = treeService.queryTreeList();
+        super.writeJson(treeList,response);
+    }
+}
---
+++ shop-backstage-consumer/src/main/java/com/jk/service/ITreeService.java
@@ -0,0 +1,13 @@
+package com.jk.service;
+
+import com.jk.model.Tree;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/4/8.
+ */
+public interface ITreeService {
+
+    public List<Tree> queryTreeList();
+}

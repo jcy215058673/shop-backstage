a48b118f3e729def6cae71cdf5bc3f325f839911
Jiang Chaoyang <215058673@qq.com>
2018-04-09 16:13:03 +0800

    首次提交框架

---
+++ shop-backstage-provider/src/main/java/com/jk/dao/TreeMapper.java
@@ -0,0 +1,13 @@
+package com.jk.dao;
+
+import com.jk.model.Tree;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/4/8.
+ */
+public interface TreeMapper {
+
+    public List<Tree> queryTreeList();
+}
